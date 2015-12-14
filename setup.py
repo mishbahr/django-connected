1ed232e0ce23f0fa7dac02c1919532d2003b1bc8
Mishbah Razzaque <mishbah@jp74.com>
2015-12-14 13:41:53 +0000

    Update Development Status

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     zip_safe=False,
     keywords='django-connected, social auth, oauth, oauth2, facebook, twitter, google',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
